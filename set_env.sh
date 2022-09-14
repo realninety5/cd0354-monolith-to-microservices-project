8dacc3c3dae6e299e331829632f48bb322c1adf5
Kingsley Godwin <kingsleyog95@gmail.com>
2022-09-14 19:03:15 +0100

    configured env variables

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,17 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=arn:aws:s3:::okahbuckss
+export URL=http://localhost:8100
+
+
+
+export POSTGRES_HOST=udagrams.cx1vg4tj6a7z.us-east-1.rds.amazonaws.com
+
+export POSTGRES_USERNAME=udagrams
+export POSTGRES_PASSWORD=udagrams
+export JWT_SECRET=tekken
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=testing
-export URL=http://localhost:8100
+export AWS_BUCKET=okahbuckss
+
